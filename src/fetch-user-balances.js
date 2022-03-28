13a9d952f3d8899bc63026084d1d7c88d792e409
luigy-lemon <luigy-lemon@protonmail.com>
2022-03-28 03:24:59 +0100

    Major optimizations at least 20x faster. Ability to use authentication in the RPC for private rpc nodes. Fixing dependencies

--- src/fetch-user-balances.js
+++ src/fetch-user-balances.js
@@ -1,88 +1,123 @@
 import fs from "fs";
+import util from "util";
 import { erc20, IncentivesContract } from "./web3.js";
 import addresses from "./contract-addresses.js";
 import { fetchAllUsers } from "./subgraph-queries.js";
-import dotenv from "dotenv";
-dotenv.config();
+import 'dotenv/config';
+
+let blockTarget = (process.env.BLOCK !== 'latest') ? Number(process.env.BLOCK) : process.env.BLOCK
 
 function sleep(ms) {
-  return new Promise( res => setTimeout(res, ms));
+    return new Promise(res => setTimeout(res, ms));
 }
 
 let stream = fs.createWriteStream("user-balances.json", { flags: "a" });
 
 async function write_userBalances(data) {
-  const users = await fetchAllUsers();
-  console.log(users.length);
-  await getUserBalances(users);
+    const users = await fetchAllUsers();
+    console.log(users.length);
+    await getUserBalances(users);
 }
 
 let userBalances = [];
 
 async function getUserBalances(users) {
-  const finished = await iterativeWeb3Query(users);
-  if (finished) {
-    return userBalances;
-  }
+    const finished = await iterativeWeb3Query(users);
+    if (finished) {
+        return userBalances;
+    }
 }
 
 let lines = 0;
 
-async function getUserBalance(user) {
-  let multipleBalances = {};
 
-  let getBalanceForToken = async t => {
-    while (true) {
-      try {
-        return await t.balanceOf(user,{blockTag:process.env.BLOCK});
-      }
-      catch (e) {
-        console.log(e.message);
-        await sleep(2000);
-      }
+
+async function getUserBalance(user) {
+    let multipleBalances = {};
+
+    let getBalanceForToken = async t => {
+        while (true) {
+            try {
+                return await t.balanceOf(user, { blockTag: blockTarget });
+            }
+            catch (e) {
+                console.log(e.message);
+                await sleep(300);
+            }
+        }
     }
-  }
-  for (let token in addresses.tokens) {
-    let agToken = erc20(addresses.tokens[token].agToken);
-    let varToken = erc20(addresses.tokens[token].varDebt);
-    let stbToken = erc20(addresses.tokens[token].stbDebt);
-    let agTokenBalance = await getBalanceForToken(agToken);
-    let varTokenBalance = await getBalanceForToken(varToken);
-    let stbTokenBalance = await getBalanceForToken(stbToken);
-    multipleBalances[`ag${token}`] = agTokenBalance.toString();
-    multipleBalances[`var${token}`] = varTokenBalance.toString();
-    multipleBalances[`stb${token}`] = stbTokenBalance.toString();
-  }
-  return multipleBalances;
+
+    async function tokenLoop(token) {
+        let agToken = erc20(addresses.tokens[token].agToken);
+        let agTokenBalance = getBalanceForToken(agToken);
+        let varToken = erc20(addresses.tokens[token].varDebt);
+        let varTokenBalance = getBalanceForToken(varToken);
+        let stbToken = erc20(addresses.tokens[token].stbDebt);
+        let stbTokenBalance = getBalanceForToken(stbToken);
+        let incomplete = true ;
+        while (incomplete) {
+            try {         
+                if (!util.inspect(agTokenBalance).includes("pending") && !util.inspect(varTokenBalance).includes("pending") &&  !util.inspect(stbTokenBalance).includes("pending")) {
+                    agTokenBalance.then(x => multipleBalances[`ag${token}`] =  x.toString())  
+                    varTokenBalance.then(x => multipleBalances[`var${token}`] =  x.toString())
+                    stbTokenBalance.then(x => multipleBalances[`stb${token}`] =  x.toString())                  
+                    incomplete = false
+                }
+                throw 'not yet'
+            }
+            catch (e) {
+                await sleep(100);
+            }
+        }   
+    }
+
+    for (let token in addresses.tokens) {
+        tokenLoop(token)  
+    }   
+    console.time('up');
+    let incomplete = true ;
+        while (incomplete) {
+            try {         
+                if (Object.keys(multipleBalances).length ===  Object.keys(addresses.tokens).length*3) {        
+                    incomplete = false
+                }
+                throw 'not yet'
+            }
+            catch (e) {
+                await sleep(200);
+            }
+        }   
+        console.timeEnd('up');
+    return multipleBalances;
 }
 
 async function iterativeWeb3Query(users) {
-  let i = 0;
-  for (const userData of users) {
-    const user = userData.id;
-    let multipleBalances = await getUserBalance(user);
-    console.log(i++, " <> ", user, " <> ", lines);
-    stream.write(
-      JSON.stringify({
-        user: user,
-        balances: multipleBalances,
-      }) + ",\n",
-      function (error) {
-        if (!error) {
-          // If the string was appended successfully:
-          lines++; // Report back there was no error
-        }
-      }
-    );
+    let i = 0;
+    for (const userData of users) {
+        const user = userData.id;
+        let multipleBalances = await getUserBalance(user);
+        console.log(i++, " <> ", user, " <> ", lines);
+        stream.write(
+            JSON.stringify({
+                user: user,
+                balances: multipleBalances,
+            }) + ",\n",
+            function (error) {
+                if (!error) {
+                    // If the string was appended successfully:
+                    lines++; // Report back there was no error
+                }
+            }
+        );
 
-    await sleep(100);
-  }
+        await sleep(100);
+    }
 
-  return true;
+    return true;
 }
 
 try {
-  write_userBalances();
+    write_userBalances();
 } catch (err) {
-  console.log(err);
+    console.log(err);
 }
--- src/web3.js
+++ src/web3.js
@@ -3,11 +3,8 @@ import ethers from "ethers";
 import IncentivesController from "./abi/incentivesController.js";
 import LendingPool from "./abi/lendingPool.js";
 import ERC20 from "./abi/ERC20.js";
-import addresses from "./contract-addresses.js"; 
-import dotenv from "dotenv";
-dotenv.config();
-
-
+import addresses from "./contract-addresses.js";
+import 'dotenv/config';
 
 // Read-Only; By connecting to a Provider, allows:
 // - Any constant function
@@ -16,12 +13,17 @@ dotenv.config();
 // - Estimating Gas for non-constant (as an anonymous sender)
 // - Static Calling non-constant methods (as anonymous sender)
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC) 
+const provider = new ethers.providers.JsonRpcProvider(
+    {
+        url: process.env.RPC_URL,
+        user: process.env.RPC_USER,
+        password: process.env.RPC_PASSWORD
+    })
 
 export const IncentivesContract = new ethers.Contract(addresses.incentivesController, IncentivesController, provider);
 
 export const lendingPool = new ethers.Contract(addresses.lendingpool, LendingPool, provider);
 
 export function erc20(tokenAddy) {
-  return new ethers.Contract(tokenAddy, ERC20, provider);
+    return new ethers.Contract(tokenAddy, ERC20, provider);
 }
